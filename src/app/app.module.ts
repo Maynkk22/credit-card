d6dd7ba053cdcc4c721bd829f26bb685b61fd0af
Mayank Yadav <mayank.yadav@manomay.biz>
2023-02-11 19:33:52 +0530

    angular reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CardFormComponent, InputComponent, CardComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/date-format-control.spec.ts
@@ -0,0 +1,7 @@
+import { DateFormatControl } from './date-format-control';
+
+describe('DateFormatControl', () => {
+  it('should create an instance', () => {
+    expect(new DateFormatControl()).toBeTruthy();
+  });
+});
